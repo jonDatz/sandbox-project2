dd87efd3f7554bcebee3c0549411a90afcd38ecd
jonDatz <notjondatz@gmail.com>
2019-03-12 13:28:18 -0400

    got help. Needs refining. Running tests

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,42 @@
+var axios = require("axios");
 var db = require("../models");
+
+// THIS WILL BE BROKEN. IT WILL NOT WORK BECAUSE QUINCY HELPED BUT WE NEED TO COMPLETE HANDLEBARS FIRST
+
+// this will allow you to change topics and make new api calls when a user picks a specific topic
+function callNewsApi(param) {
+  return axios.get(
+    // change this vvvvv to allow "param" to pick what info we send to screen
+    "https://newsapi.org/v2/top-headlines?" +
+      param +
+      "country=us&apiKey=481b5e898fd742f08451f15c0006bbdd2345"
+  );
+}
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+    // this uses the function to pick which article topic
+    callNewsApi("q=technology&")
+      .then(function(response) {
+        console.log(response);
+        // This is the code Quincy showed me
+        // res.render("index", {
+        //   data: response,
+        //   error: false
+        // });
+        var dataObj = {
+          data: response
+          // error: false
+        };
+        console.log(dataObj);
+        res.render("index", dataObj);
+      })
+      .catch(function(error) {
+        console.log(error);
+        res.render("index", { error: true });
       });
-    });
+    // nest this in axios function
   });
 
   // Load example page and pass in an example by id
