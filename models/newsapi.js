c2f614e5eeb37e76a51fc1a50982a230afa9f0da
jonDatz <notjondatz@gmail.com>
2019-03-11 20:18:25 -0400

    testing hardcoded api key

--- models/newsapi.js
+++ models/newsapi.js
@@ -1,6 +1,6 @@
 var NewsAPI = require("newsapi");
 require("dotenv").config();
-var newsapi = new NewsAPI(MY_NEWSAPI_KEY);
+var newsapi = new NewsAPI("481b5e898fd742f08451f15c0006bbdd");
 
 module.exports = function() {
   newsapi.v2
